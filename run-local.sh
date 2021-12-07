459f463d7c28d5e33568ac89734c11b6e7f0fe48
Rafi Abbel <masterraf21@gmail.com>
2021-12-07 18:05:49 +0700

    fixed

--- run-local.sh
+++ run-local.sh
@@ -1,6 +1,6 @@
-hdfs dfs -rm -f /user/root/temp/*
-hdfs dfs -rm -f /user/root/output
+hdfs dfs -rm -r -f /user/root/temp/*
+hdfs dfs -rm -r -f /user/root/output
 
-hadoop jar pat-hadoop.jar /user/root/tc1.txt /user/root/temp /user/root/output
+hadoop jar pat-hadoop.jar /user/root/tc3.txt /user/root/temp /user/root/output
 
 hdfs dfs -cat /user/root/output/*
--- setup-local.sh
+++ setup-local.sh
@@ -1,1 +1,4 @@
-hdfs dfs -mkdir -f -p /user/root
+hdfs dfs -mkdir -p /user/root
+hdfs dfs -put tc1.txt /user/root
+hdfs dfs -put tc3.txt /user/root
+hdfs dfs -put tc2.txt /user/root
